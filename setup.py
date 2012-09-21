5802004b857cebb9545737a7335103bf1e928242
Martin Martimeo <martin@martimeo.de>
2012-09-21 20:50:34 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Merodii',
+    version='0.7',
+    packages=['modules', 'helper', 'opt'],
+    url='https://github.com/MartinMartimeo/merodii',
+    license='',
+    author='Martin Martimeo',
+    author_email='martin@martimeo.de',
+    description='Ein Bot für #nsw-anime'
+)
