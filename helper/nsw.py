57e40d4e46b00d79b26d591ebf7b140a5da8df6f
Martin Martimeo <martin@martimeo.de>
2012-09-29 16:40:34 +0200

    Zeigt die nächste Sendung an

--- helper/nsw.py
+++ helper/nsw.py
@@ -6,11 +6,54 @@
 __author__ = 'Martin Martimeo <martin@martimeo.de>'
 __date__ = '21.09.12 - 19:32'
 
+import re
 import time
 
 import urllib.request, urllib.error, urllib.parse
 
 from helper.html import unescape
+
+def read_nextsendung():
+    """
+        Ließt die nächste stattfindene Sendung aus
+    """
+
+    if read_nextsendung.last < time.time() - read_nextsendung.time or not read_nextsendung.data:
+
+        # Init urllib
+        request = urllib.request.Request("http://www.nsw-anime.de/")
+        opener = urllib.request.build_opener()
+
+        # Read data
+        data = opener.open(request, None, 1)
+        rtn = ""
+        while True:
+            line = data.readline()
+            if not line:
+                break
+            try:
+                line = line.decode("utf-8")
+            except UnicodeDecodeError:
+                line = "..."
+            rtn += " " + line
+
+        # Compile Regex
+        regex = re.compile(r'<h3[^>]*>\s*NSW-Sendeplan\s*<\/h3[^>]*>\s*<ul[^>]*>\s*<li[^>]*>\s*<a[^>]*>(.*)<\/a>\s*<br[^>]*>\s*(.*)\s*<br[^>]*>\s*<\/li[^>]*>\s*', re.U + re.I)
+
+        # Parse
+        match = re.search(regex, rtn)
+        if match:
+            title = unescape(match.group(1))
+            when = unescape(match.group(2))
+            read_nextsendung.data = {'title': title, 'when': when}
+            read_nextsendung.last = time.time()
+    return read_nextsendung.data
+read_nextsendung.last = 0
+read_nextsendung.time = 60
+read_nextsendung.data = None
+
+
+
 
 
 def read_sendunginfo(url):
@@ -39,10 +82,11 @@ def read_sendunginfo(url):
         sendung_title = unescape(sendung_title.strip())
         sendung_thema = unescape(sendung_thema.strip())
         read_sendunginfo.data = {'sendung_title': sendung_title, 'sendung_thema': sendung_thema, 'sendung_start': sendung_start.strip()}
+        read_sendunginfo.last = time.time()
     return read_sendunginfo.data
 read_sendunginfo.last = 0
 read_sendunginfo.time = 60
 read_sendunginfo.data = None
 
 if __name__ == "__main__":
-    print("%s" % read_sendunginfo("http://www.nsw-anime.de/pic.php?request=text"))
+    print("%s" % read_nextsendung())
