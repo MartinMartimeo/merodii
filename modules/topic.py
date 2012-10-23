7f04fb26038ef23119d539f30bc6c63caa2c3475
Martin Martimeo <martin@martimeo.de>
2012-10-23 20:19:52 +0200

    Change Topic also when website starttime changes

--- modules/topic.py
+++ modules/topic.py
@@ -18,14 +18,17 @@ def setup(phenny):
     phenny.last_starttime = None
     phenny.last_nextwhen = None
     phenny.last_nextname = None
+    phenny.last_startsendung = None
 
     def monitor(phenny, config):
         time.sleep(5)
         while True:
 
             nsw_info = read_nswinfo(config)
-            if nsw_info["sendung_start"] != phenny.last_starttime \
+            if nsw_info["sendung_start"] != phenny.last_startsendung \
+              or nsw_info["sendung_start_time"] != phenny.last_starttime \
               or ((nsw_info["next_sendung_name"] != phenny.last_nextname or nsw_info["next_sendung_when"] != phenny.last_nextwhen) and nsw_info["sendung_mod_name"] in config.myself):
+                phenny.last_startsendung = nsw_info["sendung_start_time"]
                 phenny.last_starttime = nsw_info["sendung_start"]
                 phenny.last_nextwhen = nsw_info["next_sendung_when"]
                 phenny.last_nextname = nsw_info["next_sendung_name"]
